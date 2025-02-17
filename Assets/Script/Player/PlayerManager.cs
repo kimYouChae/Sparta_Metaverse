e0a437b57e256e97e6322eae3fb61ff79c070146
김지현 <kjhstar2486@naver.com>
2025-02-17 16:06:00 +0900

    [feat]플레이어클래스추가

--- Assets/Script/Player/PlayerManager.cs
+++ Assets/Script/Player/PlayerManager.cs
@@ -2,13 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum PlayerStateType 
-{
-    Village,            // 마을
-    MinigameOne,        // 미니게임1
-    MinigameTwo         // 미니게임2
 
-}
 
 public class PlayerManager : MonoBehaviour
 {
@@ -18,12 +12,40 @@ public class PlayerManager : MonoBehaviour
 
     [Header("===Player===")]
     [SerializeField] private Transform _playerTrs;
+    [SerializeField] private PlayerStateType _playerStateType;
+
+    [Header("===Sript===")]
+    [SerializeField] private BlockGenerate _flappyBirdGame;
 
     public Transform playerTrs => _playerTrs;
+    public PlayerStateType playerStateType => _playerStateType;
 
     private void Awake()
     {
         instance = this;
     }
+    private void Start()
+    {
+        // 처음은 마을로 설정 
+        _playerStateType = PlayerStateType.Village;
+    }
 
+    private void Update()
+    {
+        // ##TODO : 임시 
+        if (Input.GetKeyDown(KeyCode.K))
+        { 
+            _playerStateType = PlayerStateType.MinigameOne;
+            _flappyBirdGame.F_StartFlappyBird();
+
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        { 
+            _playerStateType = PlayerStateType.Village;
+        
+        }
+    }
+
+    
 }
+
